a280508257faa7ac693d197d02c3888da59542e8
zorba <zorba@sentbe.com>
2021-02-27 11:52:08 +0900

    ANTD설치, AppLayout 적용

---
+++ front/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import AppLayout from '../components/AppLayout'
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Twitter</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.12.3/antd.min.css" integrity="sha512-YDGC3dGTmRxmuvRf5gL2FcLir57pCKJMETYXydRCchEXtmhN59qZnxyoLdBVTQM8yrlcbgCRNeMbM39zM/LJxQ==" crossorigin="anonymous" />
+      </Head>
+      <AppLayout>
+        <Link href="/about">about</Link>
+        <div>HEllo Next</div>
+      </AppLayout>
+    </>
+  )
+}
+
+export default Home
