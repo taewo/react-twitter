306853d36aa544cbe664f72e8f441f86f5d9e627
zorba <zorba@sentbe.com>
2021-02-27 15:59:38 +0900

    Signup 컴포넌트 이벤트 작업

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -6,9 +6,13 @@ const Signup = () => {
   const [password, setPassword] = useState('')
   const [passwordCheck, setPasswordCheck] = useState('')
   const [term, setTerm] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [termError, setTermError] = useState(false)
 
-  const onSubmit = () => {
-    
+  const onSubmit = (e) => {
+    console.log({id, nick, password, passwordCheck, term})
+    if (password !== passwordCheck) return setPasswordError(true)
+    if (!termError) return setTermError(true)
   }
   const onChangeId = (e) => {
     setId(e.target.value)
@@ -20,15 +24,17 @@ const Signup = () => {
     setPassword(e.target.value)
   }
   const onChangePasswordChk = (e) => {
+    setPasswordError(e.target.value !== password)
     setPasswordCheck(e.target.value)
   }
   const onChangeTerm = (e) => {
+    setTermError(false)
     setTerm(e.target.value)
   }
 
   return (
     <>
-      <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+      <Form onFinish={onSubmit} style={{ padding: 10 }}>
         <div>
           <label htmlFor="user-id">아이디</label>
           <br />
@@ -48,12 +54,14 @@ const Signup = () => {
           <label htmlFor="user-password-chk">비밀번호체크</label>
           <br />
           <Input name="user-password-chk" value={passwordCheck} type="password" required onChange={onChangePasswordChk} />
+          {passwordError && <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>}
         </div>
         <div>
           <Checkbox name="user-term" value={term} onChange={onChangeTerm}>약관에 동의합니다.</Checkbox>
+          {termError && <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>}
         </div>
-        <div>
-          <Button type="primary" htmlFor="submit">가입하기</Button>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">가입하기</Button>
         </div>
       </Form>
     </>
