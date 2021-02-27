7521843ec18f1280bbce6a34bb536810990f582d
zorba <zorba@sentbe.com>
2021-02-27 12:15:25 +0900

    Profile, Signup 컴포넌트 생성

--- front/pages/index.js
+++ front/pages/index.js
@@ -11,7 +11,6 @@ const Home = () => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.12.3/antd.min.css" integrity="sha512-YDGC3dGTmRxmuvRf5gL2FcLir57pCKJMETYXydRCchEXtmhN59qZnxyoLdBVTQM8yrlcbgCRNeMbM39zM/LJxQ==" crossorigin="anonymous" />
       </Head>
       <AppLayout>
-        <Link href="/about">about</Link>
         <div>HEllo Next</div>
       </AppLayout>
     </>
