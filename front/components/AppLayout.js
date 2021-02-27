44528d562960e649b90567773e4d515b60371106
zorba <zorba@sentbe.com>
2021-02-27 19:15:06 +0900

    로그인폼 컴포넌트 생성

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
-import { Menu, Input, Button } from 'antd'
+import { Menu, Input, Button, Row, Col, Card, Avatar, Form } from 'antd'
+import LoginForm from './LoginForm'
 
 const dummy = {
   nickname: 'taewo',
@@ -21,8 +22,28 @@ const AppLayout = ({ children }) => {
           <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
         </Menu.Item>
       </Menu>
-      <Link href="/signup"><Button>회원가입</Button></Link>
-      { children }
+      <Row>
+        <Col xs={24} md={6}>
+          {dummy.isLoggedIn
+            ? <Card
+                actions={[
+                  <div key="twit">짹짹<br />{dummy.Post.length}</div>,
+                  <div key="following">팔로잉<br />{dummy.Followings.length}</div>,
+                  <div key="follower">팔로워<br />{dummy.Followers.length}</div>,
+                ]}>
+                <Card.Meta 
+                  avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+                  title={dummy.nickname}/>
+                <Link href="/signup"><Button>회원가입</Button></Link>
+              </Card>
+            : <LoginForm />
+          }
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}></Col>
+      </Row>
     </div>
   )
 }
