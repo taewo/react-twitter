a9852c07ab285b1ff8d26432e5441334e808c9a8
zorba <zorba@sentbe.com>
2021-03-01 19:52:18 +0900

    USER reducer 리팩토링

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,9 +1,9 @@
 const dummyUser = {
-  nickname: '조르바',
+  nickname: '제로초',
   Post: [],
   Followings: [],
   Followers: [],
-  signUpData: {}
+  id: 1,
 }
 
 export const initialState = {
@@ -28,65 +28,92 @@ export const LOG_IN_REQUEST = 'LOG_IN_REQUEST' // 액션의 이름
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS' // 액션의 이름
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE' // 액션의 이름
 
+export const LOAD_USER_REQUEST = 'LOAD_USER_REQUEST'
+export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS'
+export const LOAD_USER_FAILURE = 'LOAD_USER_FAILURE'
+
 export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST'
 export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS'
 export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE'
 
-export const loginAction = {
-  type: LOG_IN_REQUEST,
-  data: {
-    nickname: '조르바'
-  }
-}
+export const LOAD_FOLLOW_REQUEST = 'LOAD_FOLLOW_REQUEST'
+export const LOAD_FOLLOW_SUCCESS = 'LOAD_FOLLOW_SUCCESS'
+export const LOAD_FOLLOW_FAILURE = 'LOAD_FOLLOW_FAILURE'
 
-export const logoutAction = {
-  type: LOG_OUT_REQUEST
-}
+export const FOLLOW_USER_REQUEST = 'FOLLOW_USER_REQUEST'
+export const FOLLOW_USER_SUCCESS = 'FOLLOW_USER_SUCCESS'
+export const FOLLOW_USER_FAILURE = 'FOLLOW_USER_FAILURE'
 
-export const signupAction = (data) => {
-  return {
-    type: SIGN_UP_REQUEST,
-    data
-  }
-}
+export const UNFOLLOW_USER_REQUEST = 'UNFOLLOW_USER_REQUEST'
+export const UNFOLLOW_USER_SUCCESS = 'UNFOLLOW_USER_SUCCESS'
+export const UNFOLLOW_USER_FAILURE = 'UNFOLLOW_USER_FAILURE'
 
-const reducer = (state = initialState, action) => {
+export const REMOVE_FOLLOWER_REQUEST = 'REMOVE_FOLLOWER_REQUEST'
+export const REMOVE_FOLLOWER_SUCCESS = 'REMOVE_FOLLOWER_SUCCESS'
+export const REMOVE_FOLLOWER_FAILURE = 'REMOVE_FOLLOWER_FAILURE'
+
+export const ADD_POST_TO_ME = 'ADD_POST_TO_ME'
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case LOG_IN_REQUEST: {
       return {
         ...state,
-        isLoggedIn: true,
-        me: dummyUser,
-        isLoading: true
+        isLoggingIn: true,
+        logInErrorReason: '',
       }
     }
     case LOG_IN_SUCCESS: {
       return {
         ...state,
+        isLoggingIn: false,
         isLoggedIn: true,
         me: dummyUser,
-        isLoading: false
+        isLoading: false,
+      }
+    }
+    case LOG_IN_FAILURE: {
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+        logInErrorReason: action.error,
+        me: null,
       }
     }
     case LOG_OUT_REQUEST: {
       return {
         ...state,
         isLoggedIn: false,
-        me: null
+        me: null,
       }
     }
     case SIGN_UP_REQUEST: {
       return {
         ...state,
-        signUpData: action.data
+        isSigningUp: true,
+        isSignedUp: false,
+        signUpErrorReason: '',
+      }
+    }
+    case SIGN_UP_SUCCESS: {
+      return {
+        ...state,
+        isSigningUp: false,
+        isSignedUp: true,
+      }
+    }
+    case SIGN_UP_FAILURE: {
+      return {
+        ...state,
+        isSigningUp: false,
+        signUpErrorReason: action.error,
       }
     }
     default: {
       return {
-        ...state
+        ...state,
       }
     }
   }
 }
-
-export default reducer
