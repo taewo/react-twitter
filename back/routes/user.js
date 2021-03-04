1d3d89d190753e460a39a248386c55b04d7c3139
taewo <moondingie5@gmail.com>
2021-03-04 22:39:11 +0900

    User정보 생성하는 api 컨트롤러 작업

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const db = require('../models')
 
 router.get('/', (req, res) => {
 
 })
 
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res, next) => {
+  try {
+    const exUser = await db.User.findOne({
+      where: {
+        userId: req.body.userId
+      }
+    })
+    if (exUser) {
+      return res.status(403).send('이미 사용중인 아이디입니다.')
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12) //  salt는 10~13 사이로
+    const newUser = await db.User.create({
+      nickname: req.body.nickname,
+      userId: req.body.userId,
+      password: hashedPassword
+    })
+    console.log('newUser', newUser)
+    return res.status(200).json(newUser)
+  } catch (e) {
+    console.error(e)
+    return next(e)
+  }
 })
 
 router.get('/:id', (req, res) => {
